d00fd502b0fe71f4c027f88d1c6ef0058c74490c
Andy Mai <112735674+amai-21@users.noreply.github.com>
2024-11-02 09:25:24 -0500

    React components
    -Created a State drop down component.
    -Created a locations page.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import StateDropdown from './components/StateDropdown.tsx';
+
 function App() {
   return (
     <div className="App">
---
+++ src/components/StateDropDown.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+const states: string[] = [
+    "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", 
+    "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", 
+    "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", 
+    "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", 
+    "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", 
+    "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", 
+    "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", 
+    "Rhode Island", "South Carolina", "South Dakota", "Tennessee", 
+    "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", 
+    "Wisconsin", "Wyoming"
+];
+
+
+export const StateDropdown: React.FC = () => {
+    const [selectedState, setSelectedState] = useState<string>("");
+
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedState(event.target.value);
+    };
+
+    return (
+        <div>
+            <label htmlFor= "state">Select a State</label>
+            <select id="state" value={selectedState} onChange={handleChange}>
+                <option value="">--Choose a State--</option>
+                {states.map((state) => (
+                    <option key = {state} value= {state}>
+                        {state}
+                    </option>
+                ))}
+            </select>
+            {selectedState && <p>You selected: {selectedState}</p>}
+        </div>
+    );
+};
