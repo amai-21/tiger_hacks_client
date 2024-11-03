8959d4e841c6ba1b7f0c21d04f922af6cccecdff
Andy Mai <112735674+amai-21@users.noreply.github.com>
2024-11-02 21:37:22 -0500

    -Converted Hannah's brilliant HTML code into React components and Typescript. Thanks Hannah! -Connected the Login page with the Login Button. -Connected the location components with the Get Started Button.

---
+++ src/components/SpecializedCropsPage.tsx
@@ -0,0 +1,16 @@
+// GuideHeader.tsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../.cssFiles/SpecializedCropsHeader.css'
+
+const SpecializedCropsHeader: React.FC = () => {
+    return (
+        <header className = "SpecializedCropsHeader">
+            <div>
+                How To Garden This Plant
+            </div>
+        </header>
+    );
+};
+
+export default SpecializedCropsHeader;
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
+
+const LoginForm: React.FC = () => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    };
+
+    return (
+        <div className = "wrapper">
+            <form onSubmit = {handleSubmit}>
+            <h1>Log In</h1>
+            <div className = "input-box">
+                <input type="text" placeholder="Username" required/>
+                <FontAwesomeIcon icon = {faUser} style = {{ color: '#fff' }} />
+            </div>
+            <div className = "input-box">
+                <input type="password" placeholder="Password" required/>
+                <FontAwesomeIcon icon = {faLock} style = {{ color: '#fff' }} />
+            </div>
+
+            <div className = "remember-forgot">
+                <label><input type="checkbox" /> Remember me</label>
+                <a href="#">Forgot password?</a>
+            </div>
+
+            <button type="submit" className = "btn">Log In</button>
+
+            <div className = "register-link">
+                <p>Don't have an account? <Link to = "register.html">Register</Link>
+                </p>
+            </div>
+        </form>
+    </div>
+    );
+};
+
+export default LoginForm;
+
+    
+
+
+
+   
